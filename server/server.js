154d8008e6e7dd55a42c8bb770c9c3ec1148c10b
Guerra <miguerra@a0999b0c2691.ant.amazon.com>
2017-06-12 21:28:43 -0400

    Add geolocation support via geo api

--- server/server.js
+++ server/server.js
@@ -1,12 +1,44 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected via socket');
+
+  socket.emit('newMessage',
+  generateMessage('Admin', 'Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message) => {
+    console.log('message from client', message);
+    let { from, text } = message;
+    let createdAt = new Date().getTime();
+
+    io.emit('newMessage', generateMessage(from, text));
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    let { latitude, longitude } = coords;
+    io.emit('newLocationMessage',
+      generateLocationMessage('Admin', latitude, longitude));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log('Server is running on port 3000');
 });
