cdaacb0bec8f9b71392a50ae94f83224a91813c6
Guerra <miguerra@a0999b0c2691.ant.amazon.com>
2017-06-13 16:55:25 -0400

    add mustache

--- public/js/index.js
+++ public/js/index.js
@@ -10,19 +10,24 @@ socket.on('disconnect', () =>{
 
 socket.on('newMessage', (message) => {
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  const li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  const template = jQuery('#message-template').html();
+  const html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (message) => {
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  const li = jQuery('<li></li>');
-  const a = jQuery('<a target="_blank">My current loacation</a>');
-  li.text(`${message.from} ${formattedTime}:`);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  const template = jQuery('#location-message-template').html();
+  const html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', (e) => {
